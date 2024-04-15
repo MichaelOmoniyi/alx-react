b569ac75d05c35759ca31930e5d3a73e36a452d1
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-04-15 14:00:39 +0100

    Embedding expressions, functions

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -12,11 +12,6 @@ function App() {
       </header>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" />
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" />
-        <button>Ok</button>
       </div>
       <div className="App-footer">
         <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
