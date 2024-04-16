58e31aa3097238e73b24e71b1c7235e0ce821721
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-04-16 14:39:38 +0100

    Basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,11 +1,5 @@
-import $ from "jquery";
+const $ = require("jquery");
 
-function addParagraph() {
-    $("body").append("<p>Holberton Dashboard</p>");
-    $("body").append("<p>Dashboard data for the students</p>");
-    $("body").append("<p><Copyright - Holberton School/p>");
-}
-
-$(document).ready(() => {
-    addParagraph();
-})
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<p>Copyright - Holberton School</p>");
