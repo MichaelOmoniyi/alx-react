5d03d5645e5cc420f29bb461a134944d3f670c22
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-04-17 15:45:12 +0100

    Shapes, Loops, and Keys

---
+++ 0x02-react_props/task_5/dashboard/src/utils/utils.js
@@ -0,0 +1,15 @@
+export function getFullYear() {
+  const currentDate = new Date();
+
+  const currentYear = currentDate.getFullYear();
+
+  return currentYear;
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return "Holberton School";
+  } else {
+    return "Holberton School main dashboard";
+  }
+}
