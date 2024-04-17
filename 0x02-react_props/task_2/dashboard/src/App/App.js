ad6564ac6f470a8fdacf0b25db9247b928d15896
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-04-17 13:56:18 +0100

    Split the Notifications component

---
+++ 0x02-react_props/task_2/dashboard/src/App/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Notifications from "../Notifications/Notification";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import "./App.css";
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <Login />
+        <Footer />
+      </div>
+    </React.Fragment>
+  );
+}
