5dac3830c9fff518b8f9c3f80c649257067918fe
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-04-18 13:37:24 +0100

    Responsive design

---
+++ 0x04-React_inline_styling/task_3/dashboard/src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+
+export default function Footer() {
+  return (
+    <>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </>
+  );
+}
