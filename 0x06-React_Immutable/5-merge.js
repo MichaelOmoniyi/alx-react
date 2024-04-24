2a69e0b39af4335fe8fc92fb43a4fefaf1e8a43d
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-04-24 14:51:33 +0100

    Merge & concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,16 +1,5 @@
-const { List } = require("immutable");
-const { Map } = require("immutable");
+import { List, Map } from "immutable";
 
-export function concatElements(page1, page2) {
-  const page1 = List(page1);
-  const page2 = List(page2);
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
 
-  return page1.concat(page2);
-}
-
-export function mergeElements(page1, page2) {
-  const page1 = Map(page1);
-  const page2 = Map(page2);
-
-  return page1.mergeDeep(page2);
-}
+export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
