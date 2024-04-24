72dc706c53b392ada119c834b293cda24968b1de
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-04-24 10:45:53 +0100

    Equality

---
+++ 0x06-React_Immutable/7-equality.js
@@ -0,0 +1,16 @@
+const { Map, is } = require("immutable");
+
+export default function areMapsEqual(map1, map2) {
+  return is(map1, map2);
+}
+
+const map1 = new Map({
+  firstName: "Guillaume",
+  lastName: "Salva",
+});
+const map2 = new Map({
+  firstName: "Guillaume",
+  lastName: "Salva",
+});
+
+areMapsEqual(map1, map2);
