d923952636b832cb40ddb519c45139dcd804f38e
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-04-27 17:08:15 +0100

    Chained mutations

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,15 +1,15 @@
-const { Map } = require("immutable");
-obj = {
-  1: "Liam",
-  2: "Noah",
-  3: "Elijah",
-  4: "Oliver",
-  5: "Jacob",
-  6: "Lucas",
+const { Map } = require('immutable');
+const obj = {
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
 };
 
 export const map = Map(obj);
 
 export const map2 = map.withMutations((values) => {
-  values.set(2, "Benjamin").set(4, "Oliver");
+  values.set(2, 'Benjamin').set(4, 'Oliver');
 });
