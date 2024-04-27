b5a6409a25492c543e3059c52ae60daff23b84ea
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-04-27 17:21:15 +0100

    Ensuring eslint compliance

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,4 +1,4 @@
-const { Seq } = require("immutable");
+const { Seq } = require('immutable');
 
 function printBestStudents(obj) {
   const seq = Seq(obj);
@@ -27,28 +27,28 @@ function printBestStudents(obj) {
 const grades = {
   1: {
     score: 99,
-    firstName: "guillaume",
-    lastName: "salva",
+    firstName: 'guillaume',
+    lastName: 'salva',
   },
   2: {
     score: 100,
-    firstName: "michael",
-    lastName: "omoniyi",
+    firstName: 'michael',
+    lastName: 'omoniyi',
   },
   3: {
     score: 70,
-    firstName: "guillaume",
-    lastName: "salva",
+    firstName: 'guillaume',
+    lastName: 'salva',
   },
   4: {
     score: 66,
-    firstName: "guillaume",
-    lastName: "salva",
+    firstName: 'guillaume',
+    lastName: 'salva',
   },
   5: {
     score: 40,
-    firstName: "guillaume",
-    lastName: "salva",
+    firstName: 'guillaume',
+    lastName: 'salva',
   },
 };
 
