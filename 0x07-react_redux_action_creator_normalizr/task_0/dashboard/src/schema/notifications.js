04cd7c8e7623ddf79ec61c3ccacc7de1cae2b3c8
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-04-29 13:08:17 +0100

    Read data from a JSON

---
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -0,0 +1,6 @@
+import * as notifications from "../../../../notifications.json";
+
+export default function getAllNotificationsByUser(userId) {
+  return notifications.filter((notification) => notification.author.id === userId).map((notification) => notification.context)
+}
+
