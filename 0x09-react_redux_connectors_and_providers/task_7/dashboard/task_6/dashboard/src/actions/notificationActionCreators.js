aa6e9fa6cb007783f03c564c8a7b850450c4439c
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-05-15 02:04:03 +0100

    Connect courses: create a course selector

---
+++ 0x09-react_redux_connectors_and_providers/task_7/dashboard/task_6/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,21 @@
+import {
+  MARK_AS_READ,
+  SET_TYPE_FILTER,
+  NotificationTypeFilters,
+} from "./notificationActionTypes";
+
+export function markAsAread(index) {
+  return {
+    type: MARK_AS_READ,
+    index: index,
+  };
+}
+
+export const boundMarkAsAread = (index) => dispatch(markAsAread(index));
+
+export function setNotificationFilter(filter) {
+  return { type: SET_TYPE_FILTER, filter: filter[NotificationTypeFilters] };
+}
+
+export const boundSetNotificationFilter = (filter) =>
+  dispatch(setNotificationFilter(filter));
