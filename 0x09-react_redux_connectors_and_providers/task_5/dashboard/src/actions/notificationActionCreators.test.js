54c7d5d77b7ceea63530f3ea6e0ced203a711bce
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-05-15 01:55:26 +0100

    Connect notifications: update the test suites

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.test.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.test.js
@@ -1,29 +1,61 @@
 import {
-  markAsAread,
-  setNotificationFilter,
-} from "./notificationActionCreators";
-import {
   MARK_AS_READ,
   SET_TYPE_FILTER,
+  SET_LOADING_STATE,
+  FETCH_NOTIFICATIONS_SUCCESS,
   NotificationTypeFilters,
 } from "./notificationActionTypes";
 
-describe("Test for action creators in notificationActionCreators.js", () => {
-  it("ensure the markAsAread return the right output", () => {
-    const index = 1;
+import {
+  markAsAread,
+  setNotificationFilter,
+  setLoadingState,
+  setNotifications,
+  fetchNotifications,
+} from "./notificationActionCreators";
 
-    expect(markAsAread(index)).toEqual({
+describe("action creators tests", function () {
+  it("returns correct action for markAsRead", function () {
+    const expectedReturn = {
       type: MARK_AS_READ,
       index: 1,
-    });
+    };
+
+    const result = markAsAread(1);
+
+    expect(result).toEqual(expectedReturn);
   });
 
-  it("ensure the markAsAread return the right output", () => {
-    const filter = DEFAULT;
-
-    expect(setNotificationFilter(NotificationTypeFilters[filter])).toEqual({
+  it("returns correct action for setNotificationFilter", function () {
+    const expectedReturn = {
       type: SET_TYPE_FILTER,
       filter: "DEFAULT",
-    });
+    };
+
+    const result = setNotificationFilter(NotificationTypeFilters.DEFAULT);
+
+    expect(result).toEqual(expectedReturn);
+  });
+  it("returns correct action for setLoadingState", function () {
+    const expectedReturn = {
+      type: SET_LOADING_STATE,
+      loading: true,
+    };
+
+    const result = setLoadingState(true);
+
+    expect(result).toEqual(expectedReturn);
+  });
+  it("returns correct action for setNotifications", function () {
+    const data = { 1: { a: "Hello" }, 2: { b: "There" } };
+
+    const expectedReturn = {
+      type: FETCH_NOTIFICATIONS_SUCCESS,
+      data,
+    };
+
+    const result = setNotifications(data);
+
+    expect(result).toEqual(expectedReturn);
   });
 });
